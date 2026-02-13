6ac1eeba2fa330430c88a3d214242072c55ade53
zenith-dev-admin <admin@zenithcodex.com>
2026-02-12 21:27:29 -0300

    fix: layout and bot

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // OBRIGATÓRIO para o seu Dockerfile funcionar
 };
 
 export default nextConfig;
